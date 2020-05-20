eb76c1d3b1d2da417220003ade1223e129e221de
Chris van Riel <c.van.riel@amsterdam.nl>
2020-05-20 18:34:43 +0200

    Provenance: added 'message show provenance [url or file]'
    To retrieve the values added in the "provenance" element in a dataschema, at dataset, table or columns level. The cmd generates an object (json dictionary) of all founded values for provenance. This can be used for automating dataprocessing (creating objects in a DB) where the provenance specifiction is used to translate source column names to the specificied column names.

--- schematools/utils.py
+++ schematools/utils.py
@@ -70,3 +70,17 @@ def schema_def_from_file(filename) -> Dict[str, types.DatasetSchema]:
     with open(filename, "r") as file_handler:
         schema_info = json.load(file_handler)
         return {schema_info["id"]: types.DatasetSchema.from_dict(schema_info)}
+
+
+def schema_fetch_url_file(schema_url_file):
+    """Return schemadata from URL or File"""
+
+    if not schema_url_file.startswith("http"):
+        with open(schema_url_file) as f:
+            schema_location = json.load(f)
+    else:
+        response = requests.get(schema_url_file)
+        response.raise_for_status()
+        schema_location = response.json()
+
+    return schema_location
---
+++ tests/test_provanence.py
@@ -0,0 +1,79 @@
+from schematools.introspect.geojson import geojson_to_table
+from schematools.provenance.create import ProvenaceIteration
+
+
+def test_provenance_result():
+    """Prove that ``provenance`` elements are destillerated from input argument"""
+
+    test_data = {
+        "type": "dataset",
+        "id": "winkelgebieden",
+        "title": "winkelgebieden",
+        "status": "beschikbaar",
+        "description": "Winkelgebieden",
+        "version": "0.0.1",
+        "crs": "EPSG:28992",
+        "tables": [
+            {
+                "id": "winkelgebieden",
+                "type": "table",
+                "schema": {
+                    "$schema": "http://json-schema.org/draft-07/schema#",
+                    "type": "object",
+                    "additionalProperties": "false",
+                    "required": ["schema", "id"],
+                    "display": "id",
+                    "properties": {
+                        "schema": {
+                            "$ref": "https://schemas.data.amsterdam.nl/schema@v1.1.1#/definitions/schema"
+                        },
+                        "id": {"type": "integer"},
+                        "wkb geometry": {
+                            "$ref": "https://geojson.org/schema/Geometry.json"
+                        },
+                        "translated_colname": {
+                            "type": "string",
+                            "provenance": "source_colname",
+                        },
+                        "translated_colname2": {
+                            "type": "string",
+                            "provenance": "source_colname2",
+                        },
+                        "translated_colname3": {
+                            "type": "string",
+                            "provenance": "source_colname3",
+                        },
+                        "translated_colname4": {
+                            "type": "string",
+                            "provenance": "source_colname4",
+                        },
+                    },
+                },
+            }
+        ],
+    }
+
+    def count_num_of_levels(data):
+        """Always three levels: dataset, table and property, even if empty """
+        if not data:
+            return 1
+
+        else:
+
+            if type(data) is dict:
+                return 1 + max(count_num_of_levels(data[item]) for item in data)
+
+            if type(data) is list:
+                return 0 + max(count_num_of_levels(item) for item in data)
+            else:
+                return 0
+
+    result = ProvenaceIteration(test_data)
+    result = result.final_dic
+    print(result)
+    result = count_num_of_levels(result)
+    print(result)
+    assert result == 3  # always three levels: dataset, table and property
+
+
+print(test_provenance_result())
