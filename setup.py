11aaffb10a8d114febad30f48cf0816f8f6d0680
cvriel <chrisvanriel@gmail.com>
2020-05-27 13:30:06 +0200

    Bump version from 0.8.2 to 0.8.2

--- setup.py
+++ setup.py
@@ -15,7 +15,7 @@ def read(filename):
 
 setup(
     name="amsterdam-schema-tools",
-    version="0.8.1",
+    version="0.8.2",
     url="https://github.com/amsterdam/schema-tools",
     license="Mozilla Public 2.0",
     author="Jan Murre",
