4d6f76276085831b129eb6f4046137568b02453e
Nick Garanko <ngaranko@gmail.com>
2020-06-09 16:43:34 +0200

    Bump version to 0.8.3

--- setup.py
+++ setup.py
@@ -15,7 +15,7 @@ def read(filename):
 
 setup(
     name="amsterdam-schema-tools",
-    version="0.8.2",
+    version="0.8.3",
     url="https://github.com/amsterdam/schema-tools",
     license="Mozilla Public 2.0",
     author="Jan Murre",
