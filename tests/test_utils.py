b125c7edb7bda66713986ef16b50310f4d1bab13
jjmurre <jan.murre@gmail.com>
2020-06-22 15:06:03 +0200

    Fix to camelcasing edge case: perJaarPerM2
    Should become: per_jaar_per_m2
    Exception to the rule that postfix numbers should have underscore: watVreemd1 -> wat_vreemd_1
    New extra rule: capital + number = no extra underscore separator

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -27,3 +27,4 @@ def test_to_snake_case():
     assert to_snake_case("test_name_magic") == "test_name_magic"
     assert to_snake_case("verlengingSluitingstijd1") == "verlenging_sluitingstijd_1"
     assert to_snake_case("numbers33inTheMiddle44") == "numbers_33_in_the_middle_44"
+    assert to_snake_case("perJaarPerM2") == "per_jaar_per_m2"
