fee24a48783d43f3e4c9f0c55f84b5dde622e042
jjmurre <jan.murre@gmail.com>
2020-06-25 14:18:01 +0200

    Make env var for schemadefs more consistent

--- schematools/contrib/django/cli.py
+++ schematools/contrib/django/cli.py
@@ -11,8 +11,10 @@ settings.configure(
     DEBUG=True,
     INSTALLED_APPS=["schematools.contrib.django"],
     SCHEMA_URL=env.str("SCHEMA_URL"),
-    AMSTERDAM_SCHEMA = {"geosearch_disabled_datasets": ["bag"]},
-    SCHEMA_DEFS_URL = env.str("SCHEMA_DEFS_URL", "https://schemas.data.amsterdam.nl/schema")
+    AMSTERDAM_SCHEMA={"geosearch_disabled_datasets": ["bag"]},
+    SCHEMA_DEFS_URL=env.str(
+        "SCHEMA_DEFS_URL", "https://schemas.data.amsterdam.nl/schema"
+    ),
 )
 django.setup()
 
