01cf364bda02e9d117b56e579adfc17344ff7271
jjmurre <jan.murre@gmail.com>
2020-07-02 16:41:56 +0200

    Fix snake_casing of hoofdroutes_u_routes

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -25,6 +25,7 @@ def test_to_snake_case():
     assert to_snake_case("TestNameMagic") == "test_name_magic"
     assert to_snake_case("testNameMagic") == "test_name_magic"
     assert to_snake_case("test_name_magic") == "test_name_magic"
+    assert to_snake_case("hoofdroutes_u_routes") == "hoofdroutes_u_routes"
     assert to_snake_case("verlengingSluitingstijd1") == "verlenging_sluitingstijd_1"
     assert to_snake_case("numbers33inTheMiddle44") == "numbers_33_in_the_middle_44"
     assert to_snake_case("perJaarPerM2") == "per_jaar_per_m2"
