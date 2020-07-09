44d95e921dcf394b5c7ef2028af565656ea4e0bb
jjmurre <jan.murre@gmail.com>
2020-07-09 15:18:18 +0200

    Bump to 0.9.4

--- setup.py
+++ setup.py
@@ -15,7 +15,7 @@ def read(filename):
 
 setup(
     name="amsterdam-schema-tools",
-    version="0.9.3",
+    version="0.9.4",
     url="https://github.com/amsterdam/schema-tools",
     license="Mozilla Public 2.0",
     author="Jan Murre",
