44255359c9f7aba11f117637d48cd9dc59b539d1
Johan van Doornik <vandoornik@gmail.com>
2020-10-16 15:16:07 +0200

    dataset level en table level permissions getest

--- tests/test_permissions.py
+++ tests/test_permissions.py
@@ -12,29 +12,53 @@ def test_permissions_apply(here, engine, gebieden_schema, dbsession):
     importer = NDJSONImporter(gebieden_schema, engine)
     importer.generate_tables("bouwblokken", truncate=True)
     importer.load_file(ndjson_path)
+    importer.generate_tables("buurten", truncate=True)
+    """
     acl_data = query.get_all_table_acls(engine, schema='public')
     tablenames = set([str(entry.name) for entry in acl_data])
+    
+    #  Check if the tables have been created
     assert "gebieden_bouwblokken" in tablenames
+    assert "gebieden_buurten" in tablenames
+    
+    # Check if there is no current ACL 
     for entry in acl_data:
         if entry.name.startswith("gebieden_"):
             assert entry.acl is None
+    """
+
+    # Setup schema and profile
     ams_schema = {gebieden_schema.id: gebieden_schema}
     profile_path = here / "files" / "profiles" / "gebieden_test.json"
     with open(profile_path) as f:
         profile = json.load(f)
     profiles = {profile["name"]: profile}
 
+    # Create postgres roles
     create_role(engine, "level_a")
     create_role(engine, "level_b")
     create_role(engine, "level_c")
 
+    # Check if the roles exist, the tables exist, and the roles have no read privilige on the tables.
+    check_table_permission_denied(engine, "level_a", "gebieden_bouwblokken")
+    check_table_permission_denied(engine, "level_b", "gebieden_bouwblokken")
+    check_table_permission_denied(engine, "level_c", "gebieden_bouwblokken")
+    check_table_permission_denied(engine, "level_a", "gebieden_buurten")
+    check_table_permission_denied(engine, "level_b", "gebieden_buurten")
+    check_table_permission_denied(engine, "level_c", "gebieden_buurten")
+
+    # Apply the permissions from Schema and Profiles.
     apply_schema_and_profile_permissions(engine, ams_schema, profiles, "level_a", "LEVEL/A")
     apply_schema_and_profile_permissions(engine, ams_schema, profiles, "level_b", "LEVEL/B")
     apply_schema_and_profile_permissions(engine, ams_schema, profiles, "level_c", "LEVEL/C")
 
+    # Check if the read priviliges are correct
     check_table_permission_denied(engine, "level_a", "gebieden_bouwblokken")
     check_table_permission_granted(engine, "level_b", "gebieden_bouwblokken")
     check_table_permission_denied(engine, "level_c", "gebieden_bouwblokken")
+    check_table_permission_granted(engine, "level_a", "gebieden_buurten")
+    check_table_permission_denied(engine, "level_b", "gebieden_buurten")
+    check_table_permission_denied(engine, "level_c", "gebieden_buurten")
 
 
 def create_role(engine, role):
@@ -54,7 +78,7 @@ def check_table_permission_denied(engine, role, table):
             connection.execute("SET ROLE {}".format(role))
             connection.execute("SELECT * FROM {}".format(table))
             connection.execute("RESET ROLE")
-    assert "permission denied for table gebieden_bouwblokken" in str(e_info)
+    assert "permission denied for table {}".format(table) in str(e_info)
 
 
 def check_table_permission_granted(engine, role, table):
@@ -63,5 +87,5 @@ def check_table_permission_granted(engine, role, table):
         connection.execute("SET ROLE {}".format(role))
         result = connection.execute("SELECT * FROM {}".format(table))
         connection.execute("RESET ROLE")
-    rows = [row for row in result]
-    assert len(rows) > 0
+    assert result
+
