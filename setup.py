786d36ab429cfbd8decda849fa0f27d7c83b864e
cvriel <chrisvanriel@gmail.com>
2020-11-26 14:13:57 +0100

    bump to 15.6

--- setup.py
+++ setup.py
@@ -16,7 +16,7 @@ def read(filename):
 
 setup(
     name="amsterdam-schema-tools",
-    version="0.15.5",
+    version="0.15.6",
     url="https://github.com/amsterdam/schema-tools",
     license="Mozilla Public 2.0",
     author="Jan Murre",
