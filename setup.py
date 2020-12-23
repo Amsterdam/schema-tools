f1f150c6d483b4c7de6ee6dce8517ee44b34cd4d
cvriel <chrisvanriel@gmail.com>
2020-12-23 14:18:07 +0100

    bump to 0.16.3
    current version on pypi is 0.16.2

--- setup.py
+++ setup.py
@@ -16,7 +16,7 @@ def read(filename):
 
 setup(
     name="amsterdam-schema-tools",
-    version="0.16.1",
+    version="0.16.3",
     url="https://github.com/amsterdam/schema-tools",
     license="Mozilla Public 2.0",
     author="Jan Murre",
