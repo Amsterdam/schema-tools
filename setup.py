cd0d17ed20751ab57ce6e90ba13fb25cea866b9d
jjmurre <jan.murre@gmail.com>
2021-01-27 16:22:35 +0100

    Bump to 0.17.0
    Minor version bump, because relation change is relatively big.

--- setup.py
+++ setup.py
@@ -15,7 +15,7 @@ def read(filename):
 
 setup(
     name="amsterdam-schema-tools",
-    version="0.16.6",
+    version="0.17.0",
     url="https://github.com/amsterdam/schema-tools",
     license="Mozilla Public 2.0",
     author="Jan Murre",
