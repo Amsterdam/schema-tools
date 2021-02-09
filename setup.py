1251f771d5e5afcc8173aa1bbbcb7fccaecb847f
Chris van Riel <c.van.riel@amsterdam.nl>
2021-02-09 12:42:55 +0100

    [DS-591] bumped version

--- setup.py
+++ setup.py
@@ -15,7 +15,7 @@ def read(filename):
 
 setup(
     name="amsterdam-schema-tools",
-    version="0.17.2",
+    version="0.17.3",
     url="https://github.com/amsterdam/schema-tools",
     license="Mozilla Public 2.0",
     author="Jan Murre",
