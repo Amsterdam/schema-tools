3d859547def1048cc4bd81a670f7730d4d0c0700
jjmurre <jan.murre@gmail.com>
2021-02-25 17:38:27 +0100

    Bump to 0.17.5

--- setup.py
+++ setup.py
@@ -17,7 +17,7 @@ def read(filename: Union[Path, str]) -> str:
 
 setup(
     name="amsterdam-schema-tools",
-    version="0.17.4",
+    version="0.17.5",
     url="https://github.com/amsterdam/schema-tools",
     license="Mozilla Public 2.0",
     author="Jan Murre",
