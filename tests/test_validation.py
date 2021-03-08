05a0eefde2ee01b149bf5d6ff2b70f8ce72dc610
Guido Kollerie <guido@kollerie.com>
2021-03-08 15:04:00 +0100

    Fix bug in ValidationException.
    Added validator `IdentPropRefsValidator` that was alluded to in module
    level docstring but was not included in the original PR.

--- tests/test_validation.py
+++ tests/test_validation.py
@@ -1,10 +1,12 @@
+from collections import Callable
+
 import pytest
 
 from schematools.types import DatasetSchema
-from schematools.validation import PsqlIdentifierLengthValidator
+from schematools.validation import IdentPropRefsValidator, PsqlIdentifierLengthValidator
 
 
-def test_PsqlIdentifierLengthValidator(schema_json) -> None:
+def test_PsqlIdentifierLengthValidator(schema_json: Callable[[str], dict]) -> None:
     dataset = DatasetSchema.from_dict(schema_json("long_ids.json"))
     validator = PsqlIdentifierLengthValidator(dataset=dataset)
     error = next(validator.validate())
@@ -12,6 +14,20 @@ def test_PsqlIdentifierLengthValidator(schema_json) -> None:
 
     dataset = DatasetSchema.from_dict(schema_json("stadsdelen.json"))
     validator = PsqlIdentifierLengthValidator(dataset=dataset)
+    with pytest.raises(StopIteration):
+        # no validation error
+        next(validator.validate())
+
+
+def test_IdentPropRefsValidator(schema_json: Callable[[str], dict]) -> None:
+    dataset = DatasetSchema.from_dict(schema_json("identifier_ref.json"))
+    validator = IdentPropRefsValidator(dataset=dataset)
+    error = next(validator.validate())
+    assert error
+    assert "foobar" in error.message
+
+    dataset = DatasetSchema.from_dict(schema_json("stadsdelen.json"))
+    validator = IdentPropRefsValidator(dataset=dataset)
     with pytest.raises(StopIteration):
         # no validation error
         next(validator.validate())
