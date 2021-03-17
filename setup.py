21bff522ec0ffa69fe1139957f2188fa275600b5
Nick Garanko <ngaranko@gmail.com>
2021-03-17 15:06:52 +0100

    Bump version to 0.18.0

--- setup.py
+++ setup.py
@@ -17,7 +17,7 @@ def read(filename: Union[Path, str]) -> str:
 
 setup(
     name="amsterdam-schema-tools",
-    version="0.17.11",
+    version="0.18.0",
     url="https://github.com/amsterdam/schema-tools",
     license="Mozilla Public 2.0",
     author="Amsterdam Data en Informatie",
