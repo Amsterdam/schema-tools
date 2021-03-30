02b7b6d574e2dbcda8f6904b0efafd4e9efad5f4
Diederik van der Boor <vdboor@edoburu.nl>
2021-03-30 13:12:18 +0200

    fix create_tables() type annotations, and add missing base_app_name parameter

--- src/schematools/contrib/django/db.py
+++ src/schematools/contrib/django/db.py
@@ -1,12 +1,17 @@
+from typing import Collection, Optional
+
 from django.db import connection
 
-from schematools.types import DatasetSchema
-
 from .factories import schema_models_factory
+from .models import Dataset
 
 
-def create_tables(dataset: DatasetSchema, tables=None):
+def create_tables(
+    dataset: Dataset,
+    tables: Optional[Collection[str]] = None,
+    base_app_name: Optional[str] = None,
+):
     """Create the database tables for a given schema."""
     with connection.schema_editor() as schema_editor:
-        for model in schema_models_factory(dataset, tables=tables):
+        for model in schema_models_factory(dataset, tables=tables, base_app_name=base_app_name):
             schema_editor.create_model(model)
