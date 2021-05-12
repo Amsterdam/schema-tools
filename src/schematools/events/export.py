0a76088a8332b234274107e3dcf5443b5b2447f0
jjmurre <jan.murre@gmail.com>
2021-05-12 11:56:18 +0200

    Add docstrings and annotate event modules

--- src/schematools/events/export.py
+++ src/schematools/events/export.py
@@ -1,7 +1,16 @@
-"""Exporter module."""
+"""Exporter module.
+
+This module facilitates the export of events from GOB database tables.
+The main objective of this module is to create full events that can be used
+to test the event-importer that is also part of the `schematools.events` package.
+
+The `schematools` cli interface had an entry to use the code in this module.
+"""
+
 from __future__ import annotations
 
 from collections import defaultdict
+from typing import Any, Generator
 
 from geoalchemy2.shape import to_shape
 from json_encoder import json
@@ -45,8 +54,13 @@ def fetch_complex_fields_info(dataset_table: DatasetTableSchema) -> dict[str, di
 
 
 def collect_nm_embed_rows(
-    dataset_id, table_id, datasets_lookup, tables, complex_fields_info, connection: Connection
-):
+    dataset_id: str,
+    table_id: str,
+    datasets_lookup: dict[str, DatasetSchema],
+    tables: dict[str, dict[str, Table]],
+    complex_fields_info: dict[str, dict],
+    connection: Connection,
+) -> dict[str:Any]:
     """Fetch row info as list of embeddable objects."""
     nm_embeds = defaultdict(lambda: defaultdict(list))
     for field_name, field_info in complex_fields_info.items():
@@ -65,7 +79,12 @@ def collect_nm_embed_rows(
     return nm_embeds
 
 
-def fetch_nm_embeds(row, table_id, nm_embed_rows, complex_fields_info):
+def fetch_nm_embeds(
+    row: dict[str, Any],
+    table_id: str,
+    nm_embed_rows: dict[str, Any],
+    complex_fields_info: dict[str, dict],
+) -> dict[str, list[dict[str, Any]]]:
     """Fetch row info as lists of embeddables."""
     nm_embeds = defaultdict(list)
     for field_name, field_info in complex_fields_info.items():
@@ -77,7 +96,9 @@ def fetch_nm_embeds(row, table_id, nm_embed_rows, complex_fields_info):
     return nm_embeds
 
 
-def fetch_1n_embeds(row, complex_fields_info):
+def fetch_1n_embeds(
+    row: dict[str, Any], complex_fields_info: dict[str, dict]
+) -> dict[str, dict[str, Any]]:
     """Fetch row info as embeddable object(s)."""
     embeddable_objs = {}
     for field_name, field_info in complex_fields_info.items():
@@ -89,20 +110,22 @@ def fetch_1n_embeds(row, complex_fields_info):
     return embeddable_objs
 
 
-def export_events(datasets, dataset_id: str, table_id: str, connection: Connection):
+def export_events(
+    datasets, dataset_id: str, table_id: str, connection: Connection
+) -> Generator[str, None, None]:
     """Export the events from the indicated dataset and table."""
     tables: dict[str, dict[str, Table]] = {}
     datasets_lookup: dict[str, DatasetSchema] = {ds.id: ds for ds in datasets}
     dataset_table: DatasetTableSchema = datasets_lookup[dataset_id].get_table_by_id(table_id)
     geo_fields = [to_snake_case(field.name) for field in dataset_table.fields if field.is_geo]
 
-    complex_fields_info = fetch_complex_fields_info(dataset_table)
+    complex_fields_info: dict[str, dict] = fetch_complex_fields_info(dataset_table)
 
     for ds_id, dataset in datasets_lookup.items():
         tables[ds_id] = tables_factory(dataset, metadata)
 
     # Collect in one go (to prevent multiple queries)
-    nm_embed_rows = collect_nm_embed_rows(
+    nm_embed_rows: dict[str, Any] = collect_nm_embed_rows(
         dataset_id, table_id, datasets_lookup, tables, complex_fields_info, connection
     )
     for r in connection.execute(tables[dataset_id][table_id].select()):
