7fcb07c7568a94a0b79c319c6681b9a8e1d2c5e7
Diederik van der Boor <vdboor@edoburu.nl>
2021-06-16 13:35:48 +0200

    tests: remove 'schema_json' fixture as .from_file() also exists

--- tests/test_validation.py
+++ tests/test_validation.py
@@ -1,34 +1,32 @@
 from __future__ import annotations
 
-from typing import Callable
-
 import pytest
 
 from schematools.types import DatasetSchema
 from schematools.validation import IdentPropRefsValidator, PsqlIdentifierLengthValidator
 
 
-def test_PsqlIdentifierLengthValidator(schema_json: Callable[[str], dict]) -> None:
-    dataset = DatasetSchema.from_dict(schema_json("long_ids.json"))
+def test_PsqlIdentifierLengthValidator(here) -> None:
+    dataset = DatasetSchema.from_file(here / "files/long_ids.json")
     validator = PsqlIdentifierLengthValidator(dataset=dataset)
     error = next(validator.validate())
     assert error
 
-    dataset = DatasetSchema.from_dict(schema_json("stadsdelen.json"))
+    dataset = DatasetSchema.from_file(here / "files/stadsdelen.json")
     validator = PsqlIdentifierLengthValidator(dataset=dataset)
     with pytest.raises(StopIteration):
         # no validation error
         next(validator.validate())
 
 
-def test_IdentPropRefsValidator(schema_json: Callable[[str], dict]) -> None:
-    dataset = DatasetSchema.from_dict(schema_json("identifier_ref.json"))
+def test_IdentPropRefsValidator(here) -> None:
+    dataset = DatasetSchema.from_file(here / "files/identifier_ref.json")
     validator = IdentPropRefsValidator(dataset=dataset)
     error = next(validator.validate())
     assert error
     assert "foobar" in error.message
 
-    dataset = DatasetSchema.from_dict(schema_json("stadsdelen.json"))
+    dataset = DatasetSchema.from_file(here / "files/stadsdelen.json")
     validator = IdentPropRefsValidator(dataset=dataset)
     with pytest.raises(StopIteration):
         # no validation error
