1f061378f962c7b726d93db02c3ec0b01ec5cab0
Yashar Robert <yashar@xs4all.nl>
2021-06-30 16:55:59 +0200

    AB#5522 Add test for importing separate table file

--- tests/test_cli.py
+++ tests/test_cli.py
@@ -3,7 +3,7 @@ from datetime import datetime
 from click.testing import CliRunner
 
 from schematools.cli import import_schema
-from schematools.models import Dataset
+from schematools.models import Dataset, Table
 
 
 def test_import_schema(engine, db_url, schemas_mock, dbsession):
@@ -15,3 +15,7 @@ def test_import_schema(engine, db_url, schemas_mock, dbsession):
     # Check which data is stored in the database
     dataset: Dataset = dbsession.query(Dataset).get("afvalwegingen")
     assert dataset.date_created == datetime(2020, 1, 13)
+
+    # Check that all tables are created.
+    table_count: int = dbsession.query(Table).filter(Table.dataset_id == dataset.id).count()
+    assert table_count == 4
