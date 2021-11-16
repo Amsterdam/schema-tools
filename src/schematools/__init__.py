456d46372fa9222edba6327a0c439fb5fa114cf9
C van Riel <c.van.riel@amsterdam.nl>
2021-11-16 18:47:20 +0100

    lowercased DB schema name to

--- src/schematools/__init__.py
+++ src/schematools/__init__.py
@@ -4,4 +4,4 @@ RELATION_INDICATOR: Final[str] = "_"
 MAX_TABLE_NAME_LENGTH: Final[int] = 63  # limitation of PostgreSQL
 TMP_TABLE_POSTFIX: Final[str] = "_new"
 TABLE_INDEX_POSTFIX: Final[str] = "_idx"
-DATABASE_SCHEMA_NAME_DEFAULT: Final[str] = "PUBLIC"
+DATABASE_SCHEMA_NAME_DEFAULT: Final[str] = "public"
