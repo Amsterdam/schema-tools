c115f4ba7117aeb2aff1a4ec07808f701e582dc2
Guido Kollerie <guido@kollerie.com>
2021-11-24 11:31:09 +0100

    AB#23973 Add validation for activeVersions and table ids.
    Version information and ids in the dataset should match that of the
    referenced tables. This validation checks that.
    
    Example invocation and output:
    
    % schema validate --schema-url=../amsterdam-schema/datasets leidingeninfrastructuur ../amsterdam-schema/schema@v1.1.1/schema.json
    Structural validation: success!
    Semantic validation:
    [ActiveVersionsValidator] Table 'bovengrondsekabels' with version number '1.0.0' does not match with id 'bovengrondseKabels' of the referenced table.
    
    [ActiveVersionsValidator] Table 'ondergrondsekabels' with version number '1.0.0' does not match with id 'ondergrondseKabels' of the referenced table.
    
    [ActiveVersionsValidator] Table 'ondergrondsekabels' with version number '1.2.0' does not match with id 'ondergrondseKabels' of the referenced table.
    
    [ActiveVersionsValidator] Version number '1.2.0' in activeVersions for table 'ondergrondseKabels' does not match with version number '1.0.0' of the referenced table.

--- tests/test_validation.py
+++ tests/test_validation.py
@@ -1,13 +1,22 @@
 from __future__ import annotations
 
+import copy
+from pathlib import Path
+from typing import cast
+
 import pytest
+from more_itertools import first
 
-from schematools.types import DatasetSchema
+from schematools.types import Json, TableVersions
 from schematools.utils import dataset_schema_from_path
-from schematools.validation import IdentPropRefsValidator, PsqlIdentifierLengthValidator
+from schematools.validation import (
+    ActiveVersionsValidator,
+    IdentPropRefsValidator,
+    PsqlIdentifierLengthValidator,
+)
 
 
-def test_PsqlIdentifierLengthValidator(here) -> None:
+def test_PsqlIdentifierLengthValidator(here: Path) -> None:
     dataset = dataset_schema_from_path(here / "files/long_ids.json")
     validator = PsqlIdentifierLengthValidator(dataset=dataset)
     error = next(validator.validate())
@@ -20,7 +29,7 @@ def test_PsqlIdentifierLengthValidator(here) -> None:
         next(validator.validate())
 
 
-def test_IdentPropRefsValidator(here) -> None:
+def test_IdentPropRefsValidator(here: Path) -> None:
     dataset = dataset_schema_from_path(here / "files/identifier_ref.json")
     validator = IdentPropRefsValidator(dataset=dataset)
     error = next(validator.validate())
@@ -29,6 +38,33 @@ def test_IdentPropRefsValidator(here) -> None:
 
     dataset = dataset_schema_from_path(here / "files/stadsdelen.json")
     validator = IdentPropRefsValidator(dataset=dataset)
+    with pytest.raises(StopIteration):
+        # no validation error
+        next(validator.validate())
+
+
+def test_ActiveVersionsValidator(here: Path) -> None:
+    dataset = dataset_schema_from_path(here / "files/gebieden_sep_tables/dataset.json")
+    table_version = cast(TableVersions, first(dataset["tables"]))
+    table_version.id = table_version.id.capitalize()
+    validator = ActiveVersionsValidator(dataset=dataset)
+    error = next(validator.validate())
+    assert error
+    assert "does not match with id" in error.message
+
+    dataset = dataset_schema_from_path(here / "files/gebieden_sep_tables/dataset.json")
+    table_version = cast(TableVersions, first(dataset["tables"]))
+    for version in table_version.active:
+        incorrect_version = copy.deepcopy(version)
+        incorrect_version.major += 1
+        cast(dict[str, Json], table_version.active[version])["version"] = str(incorrect_version)
+    validator = ActiveVersionsValidator(dataset=dataset)
+    error = next(validator.validate())
+    assert error
+    assert "does not match with version number" in error.message
+
+    dataset = dataset_schema_from_path(here / "files/gebieden_sep_tables/dataset.json")
+    validator = ActiveVersionsValidator(dataset=dataset)
     with pytest.raises(StopIteration):
         # no validation error
         next(validator.validate())
