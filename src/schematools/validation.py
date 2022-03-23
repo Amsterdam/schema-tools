14856a8827864bbe58843e0eae819531aa157075
Yashar Robert <yashar@xs4all.nl>
2022-03-23 16:17:47 +0100

    AB40635 add crs validation

--- src/schematools/validation.py
+++ src/schematools/validation.py
@@ -256,6 +256,19 @@ def _check_maingeometry(dataset: DatasetSchema) -> Iterator[str]:
             yield str(e)
 
 
+@_register_validator("crs")
+def _check_crs(dataset: DatasetSchema) -> Iterator[str]:
+    """Check dataset has crs property if any of its tables has a geo field."""
+    if dataset.data.get("crs") is None:
+        for table in dataset.tables:
+            for field in table.get_fields():
+                if field.is_geo:
+                    yield (
+                        "A 'crs' field is required for datasets with geometric data. "
+                        'suggestion: "EPSG:28992".'
+                    )
+
+
 @_register_validator("property formats")
 def _property_formats(dataset: DatasetSchema) -> Iterator[str]:
     """Properties should have a valid "format", or none at all."""
