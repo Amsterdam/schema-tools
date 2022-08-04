61bc3c31f6b615b9eedad4a7135cabe2fafe2b35
Jan Murre <jan.murre@gmail.com>
2022-08-04 12:05:37 +0200

    Update src/schematools/contrib/django/faker/providers/intwindow.py
    Co-authored-by: Diederik van der Boor <vdboor@edoburu.nl>

--- src/schematools/contrib/django/faker/providers/intwindow.py
+++ src/schematools/contrib/django/faker/providers/intwindow.py
@@ -8,7 +8,7 @@ class IntWindowProvider(BaseProvider):  # noqa: D101
     """Provider for an integer with an min- and max-value."""
 
     def int_window(self, min_value: int = 1800, max_value: int = 2000) -> str:  # noqa: D102
-        str(secrets.choice(range(min_value, max_value)))
+        return str(secrets.choice(range(min_value, max_value)))
 
 
 Faker.add_provider(IntWindowProvider, locale="nl_NL")
