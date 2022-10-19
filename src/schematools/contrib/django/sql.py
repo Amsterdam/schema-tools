e67043a8bda989cc0c7e22d39a26c524595c6c94
jjmurre <jan.murre@gmail.com>
2022-10-19 11:09:14 +0200

    Removed the sql generation for the relate command
    This makes no sense, because relate can only work on actual tables.
    It would be weird to generate the tables, and only generate sql
    for the additions of the relations.

--- src/schematools/contrib/django/sql.py
+++ src/schematools/contrib/django/sql.py
@@ -21,18 +21,3 @@ def get_sql_for(objects: List[DynamicModel]) -> None:
         statements = compiler.as_sql()
         for statement, params in statements:
             yield cursor.mogrify(statement, params).decode() + ";"
-
-
-def get_bulk_update_sql_for(objects: List[DynamicModel], field_names: List[str]) -> None:
-    """Get the SQL update statements for the provided model objects."""
-    cursor = connection.cursor()
-    for obj in objects:
-        values = {field_name: getattr(obj, field_name) for field_name in field_names}
-        query = sql.UpdateQuery(obj)
-        query.add_update_values(values)
-        query.where = query.where_class()
-        # query.add_q(Q(pk__in=pk_list[offset : offset + GET_ITERATOR_CHUNK_SIZE]))
-        query.add_q(Q(pk=obj.pk))
-        compiler = query.get_compiler("default")
-        statement, params = compiler.as_sql()
-        yield cursor.mogrify(statement, params).decode() + ";"
