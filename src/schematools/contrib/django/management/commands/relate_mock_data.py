c0e945a161243d1a2389a8cc969baa21f0adbf83
jjmurre <jan.murre@gmail.com>
2022-10-25 20:05:28 +0200

    Change cli arguments for mocking to be more intuitive
    Based on feedback by @lbam.

--- src/schematools/contrib/django/management/commands/relate_mock_data.py
+++ src/schematools/contrib/django/management/commands/relate_mock_data.py
@@ -1,3 +1,4 @@
+from pathlib import Path
 from typing import Any, Dict, List
 
 from django.conf import settings
@@ -23,18 +24,34 @@ class Command(BaseCommand):  # noqa: D101
             default=settings.SCHEMA_URL,
             help=f"Schema URL (default: {settings.SCHEMA_URL})",
         )
-        parser.add_argument("--skip", nargs="*", default=[], help="Dataset ids to be skipped.")
         parser.add_argument(
-            "--limit-to", nargs="*", default=[], help="Dataset ids to be included exclusively."
+            "--skip",
+            nargs="*",
+            default=[],
+            help="""Dataset ids to be skipped. Only applies to id-based dataset,
+            not to path-based dataset. Use a list of ids, e.g.: --skip bag fietspaaltjes""",
         )
 
     def handle(self, *args: List[Any], **options: Dict[str, Any]) -> None:  # noqa: D102
 
-        if options["schema"]:
-            schemas = [dataset_schema_from_path(filename) for filename in list(options["schema"])]
-        else:
-            schemas = get_schemas_for_url(
-                options["schema_url"], limit_to=options["limit_to"], skip=options["skip"]
+        paths = []
+        path_based_schemas = []
+        dataset_ids = []
+        id_based_schemas = []
+        paths_or_dataset_ids = options["schema"]
+
+        if paths_or_dataset_ids:
+            for path_or_dataset_id in paths_or_dataset_ids:
+                if Path(path_or_dataset_id).exists():
+                    paths.append(path_or_dataset_id)
+                else:
+                    dataset_ids.append(path_or_dataset_id)
+
+            path_based_schemas = [dataset_schema_from_path(path) for path in paths]
+
+        if dataset_ids or not paths:
+            id_based_schemas = get_schemas_for_url(
+                options["schema_url"], limit_to=dataset_ids, skip=options["skip"]
             )
 
-        relate_datasets(*schemas)
+        relate_datasets(*(path_based_schemas + id_based_schemas))
