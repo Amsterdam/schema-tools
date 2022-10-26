ad0b9e3fae10a4518b5ab959cc591a5f164d1c43
jjmurre <jan.murre@gmail.com>
2022-10-26 13:35:38 +0200

    Add schemaloader functionality for the Django mgm. commands

--- src/schematools/contrib/django/schemas.py
+++ src/schematools/contrib/django/schemas.py
@@ -5,6 +5,7 @@ from typing import List
 from more_ds.network.url import URL
 
 from schematools import types
+from schematools.datasetcollection import DatasetCollection, set_schema_loader
 from schematools.utils import dataset_schema_from_url, dataset_schemas_from_url
 
 
@@ -16,16 +17,18 @@ def get_schemas_for_url(
     schema_url: URL, limit_to: List[str] | None = None, skip: List[str] | None = None
 ) -> List[types.DatasetSchema]:
     """Gets all schemas from `schema_url`, or a subset, depending on `limit_to` and `skip`."""
+    set_schema_loader(schema_url)
+    dataset_collection = DatasetCollection()
     if not limit_to:
         schemas = [
             schema
-            for schema in dataset_schemas_from_url(schema_url, prefetch_related=True).values()
+            for schema in dataset_collection.get_all_datasets().values()
             if schema.id not in _setify(skip)
         ]
     else:
         limit_to_minus_skip = _setify(limit_to) - _setify(skip)
         schemas = [
-            dataset_schema_from_url(schema_url, schema_id, prefetch_related=True)
+            dataset_collection.get_dataset(schema_id, prefetch_related=True)
             for schema_id in limit_to_minus_skip
         ]
 
