386be307bc5834bb73841e31a82248edbdcbe285
jjmurre <jan.murre@gmail.com>
2022-10-26 14:06:51 +0200

    Exclude dataset with `enable_db` is False for mocking

--- src/schematools/contrib/django/faker/create.py
+++ src/schematools/contrib/django/faker/create.py
@@ -1,8 +1,11 @@
+import logging
 from typing import List
 
 from schematools.contrib.django.factories import schema_model_mockers_factory
 from schematools.contrib.django.models import Dataset
 from schematools.contrib.django.sql import get_sql_for
+
+logger = logging.getLogger(__name__)
 
 
 def create_data_for(
@@ -10,7 +13,8 @@ def create_data_for(
 ) -> List[str]:
     """Create mock data for the indicated datasets."""
     for dataset in datasets:
-        if not dataset.has_api_enabled(dataset.schema):
+        if not dataset.enable_db:
+            logger.warning("Skipping `%s`, `enable_db` is False", dataset.name)
             continue
         model_mockers = {
             cls._meta.get_model_class()._meta.model_name: cls
