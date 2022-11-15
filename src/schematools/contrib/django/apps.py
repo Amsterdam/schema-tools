8d90607df608069520bbb93627a240af827e6cf9
DonQueso89 <k.van.ekeren@amsterdam.nl>
2022-11-15 16:03:33 +0100

    Make django console script work in dev. Define default class for autofields

--- src/schematools/contrib/django/apps.py
+++ src/schematools/contrib/django/apps.py
@@ -5,3 +5,4 @@ class SchematoolsAppConfig(AppConfig):
     name = "schematools.contrib.django"
     # Alias as `datasets` app to avoid unnecessary migrations on legacy systems.
     label = "datasets"
+    default_auto_field = "django.db.models.BigAutoField"
