5a78dc50ced38ec98c91e1dc25220004267f36b1
Diederik van der Boor <vdboor@edoburu.nl>
2022-11-17 10:58:45 +0100

    Cleanup schematools.contrib.django.faker.create

--- src/schematools/contrib/django/faker/create.py
+++ src/schematools/contrib/django/faker/create.py
@@ -1,15 +1,17 @@
 import logging
 from typing import List
 
+from django.db import connection
+from django.db.models import sql
+
 from schematools.contrib.django.factories import schema_model_mockers_factory
-from schematools.contrib.django.models import Dataset
-from schematools.contrib.django.sql import get_sql_for
+from schematools.contrib.django.models import Dataset, DynamicModel
 
 logger = logging.getLogger(__name__)
 
 
 def create_data_for(
-    *datasets: List[Dataset], start_at: int = 1, size: int = 50, sql: bool = False
+    *datasets: Dataset, start_at: int = 1, size: int = 50, sql: bool = False
 ) -> List[str]:
     """Create mock data for the indicated datasets."""
     for dataset in datasets:
@@ -25,6 +27,21 @@ def create_data_for(
             if start_at > 1:
                 mock_model._setup_next_sequence = lambda: start_at
             if sql:
-                return get_sql_for(mock_model.build_batch(size))
+                return list(_get_sql_for(mock_model.build_batch(size)))
             else:
                 mock_model.create_batch(size)
+
+
+def _get_sql_for(objects: List[DynamicModel]):
+    """Get the SQL insert statements for the provided model objects."""
+    # We need a real cursor here, so that `cursor.mogrify`
+    # knows exactly how to render the query.
+    cursor = connection.cursor()
+    for obj in objects:
+        values = obj._meta.local_fields
+        query = sql.InsertQuery(obj)
+        query.insert_values(values, [obj])
+        compiler = query.get_compiler("default")
+        statements = compiler.as_sql()
+        for statement, params in statements:
+            yield cursor.mogrify(statement, params).decode() + ";"
