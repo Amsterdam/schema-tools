290ac34695f0b78abd29cfcfbeefc28835bfa802
Diederik van der Boor <vdboor@edoburu.nl>
2022-11-29 13:23:46 +0100

    Cleanup coordinate system usage, centralize 3D detection

--- src/schematools/__init__.py
+++ src/schematools/__init__.py
@@ -1,11 +1,28 @@
 """Schematools, a library to work with amsterdam schema files."""
 
-from typing import Final
+from typing import Final, List
 
+# Internal conventions
 RELATION_INDICATOR: Final[str] = "_"
 MAX_TABLE_NAME_LENGTH: Final[int] = 63  # limitation of PostgreSQL
 TMP_TABLE_POSTFIX: Final[str] = "_new"
 TABLE_INDEX_POSTFIX: Final[str] = "_idx"
 DATABASE_SCHEMA_NAME_DEFAULT: Final[str] = "public"
+
+# Defaults
 DEFAULT_SCHEMA_URL: Final[str] = "https://schemas.data.amsterdam.nl/datasets/"
 DEFAULT_PROFILE_URL: Final[str] = "https://schemas.data.amsterdam.nl/profiles/"
+
+# Common coordinate reference systems
+CRS_WGS84: Final[str] = "EPSG:4326"  # World Geodetic System 1984, used in GPS
+CRS_RD_NEW: Final[str] = "EPSG:28992"  # Amersfoort / RD New
+SRID_RD_NEW: Final[int] = 28992
+
+# Some likely used 3D coordinate reference systems:
+SRID_3D: Final[List[int]] = [
+    7415,  # Amersfoort / RD New + NAP height
+    7423,  # ETRS89 + EVRF2007 height
+    9286,  # ETRS89 + NAP height
+    4979,  # WGS84 + height
+    4978,  # WGS84 + geocentric height
+]
