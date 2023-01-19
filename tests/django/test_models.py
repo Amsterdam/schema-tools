367d17fd105c56335ae5c58cb38bb7d696802ba0
LB <78799392+lbam@users.noreply.github.com>
2023-01-19 14:59:03 +0100

    Revert "Skip tests that require the database if not configured"
    This reverts commit c4917bfbd70aaa3b7f898aab57103cd41ed7bb8b and removes
    every instance of skipif in the code base.

--- tests/django/test_models.py
+++ tests/django/test_models.py
@@ -13,11 +13,8 @@ from schematools.contrib.django.models import (
     LooseRelationManyToManyField,
 )
 
-from .conftest import DATABASE_URL
-
 
 @pytest.mark.django_db
-@pytest.mark.skipif(DATABASE_URL is None, reason="DATABASE_URL not set")
 def test_model_factory_fields(afval_dataset) -> None:
     """Prove that the fields from the schema will be generated"""
     table = afval_dataset.schema.tables[0]
@@ -58,7 +55,6 @@ def test_model_factory_fields(afval_dataset) -> None:
 
 
 @pytest.mark.django_db
-@pytest.mark.skipif(DATABASE_URL is None, reason="DATABASE_URL not set")
 def test_model_factory_table_name_no_versions(afval_dataset):
     """Prove that relations between models can be resolved"""
     models = {
