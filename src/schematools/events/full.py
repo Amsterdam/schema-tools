1db8878f1b34eb54c50f49e4618d47a37bcd3eae
Jaap Koetsier <jaap@chariot-it.nl>
2023-05-03 10:23:57 +0100

    Add support for loading events in batches. Extract initialisation and finalisation into separate methods to improve performance. Cache initialised tables

--- src/schematools/events/full.py
+++ src/schematools/events/full.py
@@ -5,14 +5,14 @@ import json
 import logging
 from collections import defaultdict
 
-from sqlalchemy import inspect
+from sqlalchemy import Table, inspect
 from sqlalchemy.engine import Connection
 
 from schematools.events import metadata
 from schematools.factories import tables_factory
 from schematools.importer.base import BaseImporter
 from schematools.naming import to_snake_case
-from schematools.types import DatasetSchema
+from schematools.types import DatasetSchema, DatasetTableSchema
 
 # Enable the sqlalchemy logger by uncommenting the following 2 lines to debug SQL related issues
 # logging.basicConfig()
@@ -71,6 +71,7 @@ class EventsProcessor:
         _metadata.bind = connection.engine
         inspector = inspect(connection.engine)
         self.tables = {}
+        self.full_load_tables = defaultdict(dict)
         for dataset_id, dataset in self.datasets.items():
             base_tables_ids = {dataset_table.id for dataset_table in dataset.tables}
             self.tables[dataset_id] = tfac = {
@@ -106,20 +107,13 @@ class EventsProcessor:
                 result[k] = v
         return result
 
-    def process_row(self, event_id: str, event_meta: dict, event_data: dict) -> None:
-        """Process one row of data.
-
-        Args:
-            event_id: Id of the event (Kafka id)
-            event_meta: Metadata about the event
-            event_data: Data containing the fields of the event
-        """
-        event_type = event_meta["event_type"]
-        db_operation_name, needs_select = EVENT_TYPE_MAPPINGS[event_type]
-        dataset_id = event_meta["dataset_id"]
-        table_id = event_meta["table_id"]
-
-        if event_meta.get("full_load_sequence", False):
+    def _get_full_load_tables(
+        self, dataset_id: str, table_id: str
+    ) -> tuple[Table, DatasetTableSchema]:
+        try:
+            # Cached
+            table, schema_table = self.full_load_tables[dataset_id][table_id]
+        except KeyError:
             # Initialise fresh table for full load.
             dataset_schema = self.datasets[dataset_id]
             schema_table = dataset_schema.get_table_by_id(table_id)
@@ -132,13 +126,50 @@ class EventsProcessor:
                 ind_extra_index=False,
             )
 
-            if event_meta.get("first_of_sequence", False):
-                self.conn.engine.execute(f"TRUNCATE {db_table_name}")
-
             table = importer.tables[table_id]
+            self.full_load_tables[dataset_id][table_id] = table, schema_table
+
+        return table, schema_table
+
+    def _before_process(self, event_meta: dict) -> tuple[Table, DatasetTableSchema]:
+        dataset_id = event_meta["dataset_id"]
+        table_id = event_meta["table_id"]
+
+        if event_meta.get("full_load_sequence", False):
+            table, schema_table = self._get_full_load_tables(dataset_id, table_id)
+
+            if event_meta.get("first_of_sequence", False):
+                self.conn.engine.execute(f"TRUNCATE {table.name}")
+
+            return table, schema_table
         else:
             schema_table = self.datasets[dataset_id].get_table_by_id(table_id)
             table = self.tables[dataset_id][to_snake_case(table_id)]
+            return table, schema_table
+
+    def _after_process(self, event_meta: dict, table: Table):
+        if event_meta.get("full_load_sequence", False) and event_meta.get(
+            "last_of_sequence", False
+        ):
+            dataset_id = event_meta["dataset_id"]
+            table_id = event_meta["table_id"]
+            table_to_replace = self.tables[dataset_id][to_snake_case(table_id)]
+
+            logger.info("End of full load sequence. Replacing active table.")
+            with self.conn.begin():
+                self.conn.execute(f"TRUNCATE {table_to_replace.name}")
+                self.conn.execute(
+                    f"INSERT INTO {table_to_replace.name} "  # nosec B608 # noqa: S608
+                    f"SELECT * FROM {table.name}"  # nosec B608 # noqa: S608
+                )
+                self.conn.execute(f"DROP TABLE {table.name} CASCADE")
+            self.full_load_tables[dataset_id].pop(table_id)
+
+    def _prepare_row(
+        self, event_meta: dict, event_data: dict, schema_table: DatasetTableSchema
+    ) -> dict:
+        dataset_id = event_meta["dataset_id"]
+        table_id = event_meta["table_id"]
 
         row = self._flatten_event_data(event_data)
 
@@ -150,16 +181,29 @@ class EventsProcessor:
         identifier = schema_table.identifier
         id_value = ".".join(str(row[fn]) for fn in identifier)
         row["id"] = id_value
+        return row
 
+    def _process_row(
+        self, event_meta: dict, event_data: dict, table: Table, schema_table: DatasetTableSchema
+    ) -> None:
+        """Process one row of data.
+
+        Args:
+            event_id: Id of the event (Kafka id)
+            event_meta: Metadata about the event
+            event_data: Data containing the fields of the event
+        """
+
+        row = self._prepare_row(event_meta, event_data, schema_table)
+        id_value = row["id"]
+
+        event_type = event_meta["event_type"]
+        db_operation_name, needs_select = EVENT_TYPE_MAPPINGS[event_type]
         db_operation = getattr(table, db_operation_name)()
-        id_field = (
-            table.c.id
-            if schema_table.has_composite_key
-            else getattr(table.c, schema_table.identifier[0])
-        )
 
         update_parent_op = update_parent_row = None
         if schema_table.has_parent_table:
+            dataset_id = event_meta["dataset_id"]
             # Have relation. We need to update the relation columns in the parent table as well
             rel_field_prefix = to_snake_case(schema_table.parent_table_field.name)
             parent_schema_table = schema_table.parent_table
@@ -185,6 +229,11 @@ class EventsProcessor:
                 update_parent_row = {k: None for k in update_parent_row.keys()}
 
         if needs_select:
+            id_field = (
+                table.c.id
+                if schema_table.has_composite_key
+                else getattr(table.c, schema_table.identifier[0])
+            )
             db_operation = db_operation.where(id_field == id_value)
         with self.conn.begin():
             self.conn.execute(db_operation, row)
@@ -192,23 +241,40 @@ class EventsProcessor:
             if update_parent_op is not None:
                 self.conn.execute(update_parent_op, update_parent_row)
 
-        if event_meta.get("full_load_sequence", False) and event_meta.get(
-            "last_of_sequence", False
-        ):
-            table_to_replace = self.tables[dataset_id][to_snake_case(table_id)]
-
-            logger.info("End of full load sequence. Replacing active table.")
-            with self.conn.begin():
-                self.conn.execute(f"TRUNCATE {table_to_replace.name}")
-                self.conn.execute(
-                    f"INSERT INTO {table_to_replace.name} "  # nosec B608 # noqa: S608
-                    f"SELECT * FROM {table.name}"  # nosec B608 # noqa: S608
-                )
-                self.conn.execute(f"DROP TABLE {table.name} CASCADE")
-
-    def process_event(self, event_id: str, event_meta: dict, event_data: dict):
+    def process_event(self, event_meta: dict, event_data: dict):
+        table, schema_table = self._before_process(event_meta)
         """Do inserts/updates/deletes."""
-        self.process_row(event_id, event_meta, event_data)
+        self._process_row(event_meta, event_data, table, schema_table)
+        self._after_process(event_meta, table)
+
+    def process_events(self, events: list[tuple[dict, dict]]):
+        if len(events) == 0:
+            return
+
+        first_event_meta = events[0][0]
+        last_event_meta = events[-1][0]
+        table, schema_table = self._before_process(first_event_meta)
+
+        if first_event_meta.get("full_load_sequence", False):
+            # full_load_sequence only contains add events. Take more efficient shortcut.
+            self._process_bulk_adds(events, table, schema_table)
+        else:
+            for event_meta, event_data in events:
+                self._process_row(event_meta, event_data, table, schema_table)
+
+        self._after_process(last_event_meta, table)
+
+    def _process_bulk_adds(self, events: list[tuple[dict, dict]], table, schema_table):
+        rows = []
+        for event_meta, event_data in events:
+            if event_meta["event_type"] != "ADD":
+                raise Exception("This method should only be called when processing ADD events.")
+
+            row = self._prepare_row(event_meta, event_data, schema_table)
+            rows.append(row)
+
+        with self.conn.begin():
+            self.conn.execute(table.insert(), rows)
 
     def load_events_from_file(self, events_path: str):
         """Load events from a file, primarily used for testing."""
@@ -219,7 +285,18 @@ class EventsProcessor:
                     event_meta = json.loads(event_meta_str)
                     event_data = json.loads(data_str)
                     self.process_event(
-                        event_id,
                         event_meta,
                         event_data,
                     )
+
+    def load_events_from_file_using_bulk(self, events_path: str):
+        """Load events from a file, primarily used for testing."""
+        with open(events_path) as ef:
+            events = []
+            for line in ef:
+                if line.strip():
+                    event_id, event_meta_str, data_str = line.split("|", maxsplit=2)
+                    event_meta = json.loads(event_meta_str)
+                    event_data = json.loads(data_str)
+                    events.append((event_meta, event_data))
+            self.process_events(events)
