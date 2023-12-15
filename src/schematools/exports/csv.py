3cb7e95668ea9503d75f38cf70a8ad92ebf00e19
jjmurre <jan.murre@gmail.com>
2023-12-15 10:49:30 +0100

    AB#101818 Only export actual record for csv and jsonlines
    Most users are not interested in the historical records. And, export is
    done every night. Bright export in line with the DSO API export.
    
    Changes:
    
    - Only select current records for temporal tables
    - Change headers (capitalize) to be in line with the DSO API export
      (althought it seems a bit strange to me to use capitalize)
    - Change datetime notation to use iso format
    - Foreign keys only use the identificatie (not the dot-volgnummer)
    
    To do:
    Mabye we should also export current records only for the geopackages.

--- src/schematools/exports/csv.py
+++ src/schematools/exports/csv.py
@@ -1,22 +1,42 @@
 from __future__ import annotations
 
 import csv
+from datetime import date
 from typing import IO
 
-from geoalchemy2 import functions as func  # ST_AsEWKT
-from sqlalchemy import MetaData, Table, select
+from geoalchemy2 import functions as gfunc  # ST_AsEWKT
+from sqlalchemy import MetaData, Table, func, select
 from sqlalchemy.engine import Connection
 
 from schematools.exports import BaseExporter, enable_datetime_cast
 from schematools.naming import toCamelCase
-from schematools.types import DatasetSchema
+from schematools.types import DatasetFieldSchema, DatasetSchema
 
 metadata = MetaData()
 
 
 class CsvExporter(BaseExporter):  # noqa: D101
     extension = "csv"
-    geo_modifier = staticmethod(lambda col, fn: func.ST_AsEWKT(col).label(fn))
+
+    @staticmethod
+    def geo_modifier(field: DatasetFieldSchema, column):
+        if not field.is_geo:
+            return column
+        return gfunc.ST_AsEWKT(column).label(field.db_name)
+
+    @staticmethod
+    def id_modifier(field: DatasetFieldSchema, column):
+        if field.table.is_temporal and field.is_composite_key:
+            return func.split_part(column, ".", 1).label(field.db_name)
+        return column
+
+    @staticmethod
+    def datetime_modifier(field: DatasetFieldSchema, column):
+        if field.type == "string" and field.format == "date-time":
+            return func.to_char(column, 'YYYY-MM-DD"T"HH24:MI:SS').label(field.db_name)
+        return column
+
+    processors = (geo_modifier, id_modifier, datetime_modifier)
 
     def write_rows(  # noqa: D102
         self, file_handle: IO[str], table: DatasetSchema, sa_table: Table, srid: str
@@ -24,8 +44,12 @@ class CsvExporter(BaseExporter):  # noqa: D101
         columns = list(self._get_columns(sa_table, table))
         field_names = [c.name for c in columns]
         writer = csv.DictWriter(file_handle, field_names, extrasaction="ignore")
-        writer.writerow({fn: toCamelCase(fn) for fn in field_names})
+        # Use capitalize() on headers, because csv export does the same
+        writer.writerow({fn: toCamelCase(fn).capitalize() for fn in field_names})
         query = select(self._get_columns(sa_table, table))
+        temporal_clause = self._get_temporal_clause(sa_table, table)
+        if temporal_clause is not None:
+            query = query.where(temporal_clause)
         if self.size is not None:
             query = query.limit(self.size)
         result = self.connection.execution_options(yield_per=1000).execute(query)
@@ -41,8 +65,11 @@ def export_csvs(
     table_ids: list[str],
     scopes: list[str],
     size: int,
+    temporal_date: date = date.today(),
 ):
     """Utility function to wrap the Exporter."""
     enable_datetime_cast()
-    exporter = CsvExporter(connection, dataset_schema, output, table_ids, scopes, size)
+    exporter = CsvExporter(
+        connection, dataset_schema, output, table_ids, scopes, size, temporal_date
+    )
     exporter.export_tables()
