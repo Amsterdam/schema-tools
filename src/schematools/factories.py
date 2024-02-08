2247c930dac85d10611c47f384b180c5c727e53e
Barry Daniels <mail@barrydaniels.nl>
2024-02-08 10:17:48 +0100

    Fixed issues

--- src/schematools/factories.py
+++ src/schematools/factories.py
@@ -350,7 +350,7 @@ def index_factory(
             _build_identifier_index(table, dataset_table, db_table_name),
             *_build_fk_indexes(table, dataset_table, db_table_name),
             *_build_geo_indexes(table, dataset_table, db_table_name),
-            *_build_temporal_indexes(table, dataset_table, db_table_name),
+            *build_temporal_indexes(table, dataset_table, db_table_name),
         ]
 
     through_indexes = _build_m2m_indexes(
@@ -408,35 +408,36 @@ def _build_geo_indexes(
     ]
 
 
-def _build_temporal_indexes(
+def build_temporal_indexes(
     table_object: Table, dataset_table: DatasetTableSchema, db_table_name: str
 ) -> list[Index]:
     """Creates an index on temporal fields."""
-    if dataset_table._temporal_range_field_ids:
-        fields = []
-        if dataset_table._temporal_range_field_ids:
-            for field in dataset_table._temporal_range_field_ids:
-                try:
-                    fields.append(table_object.c[to_snake_case(field)])
-                except KeyError:
-                    logger.warning(
-                        "Field '%s' not found...skipping temporal index creation", field
-                    )
-                    return []
-
-            combined_index = Index(
-                _format_index_name(f"{db_table_name}_temporal{TABLE_INDEX_POSTFIX}"), *fields
-            )
-            fields_index = [
-                Index(
-                    _format_index_name(f"{db_table_name}_{field.db_name}{TABLE_INDEX_POSTFIX}"),
-                    table_object.c[field.db_name],
+    if dataset_table.temporal:
+        fields = set()
+        for field in dataset_table._temporal_range_field_ids:
+            try:
+                fields.add(table_object.c[to_snake_case(field)])
+            except KeyError:
+                logger.warning(
+                    "Field '%s' not found...skipping temporal index creation", field
                 )
-                for field in dataset_table.temporal.temporal_fields
-            ]
-            return [combined_index, *fields_index]
-    return []
+                return []
 
+        combined_index = Index(
+            _format_index_name(f"{db_table_name}_temporal{TABLE_INDEX_POSTFIX}"), *fields
+        )
+
+        fields_index = [
+            Index(
+                _format_index_name(f"{db_table_name}_{field.db_name}{TABLE_INDEX_POSTFIX}"),
+                table_object.c[field.db_name],
+            )
+            for field in dataset_table.temporal.temporal_fields
+        ]
+
+        return [combined_index, *fields_index]
+    else:
+        return []
 
 def _build_m2m_indexes(
     metadata: MetaData,
