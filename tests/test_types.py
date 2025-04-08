06bca6a9a757cdcf162818282ec42197894f9dcd
Pieter Stokkink <p.stokkink@amsterdam.nl>
2025-04-08 12:57:18 +0200

    Add zoom props to DatasetTableSchema (#593)
    * add min_zoom and max_zoom props on table with sensible defaults
    
    * add test
    
    * bump version to v7.3.0

--- tests/test_types.py
+++ tests/test_types.py
@@ -480,6 +480,18 @@ def test_extra_properties_of_relation_field_are_also_in_through_table(gebieden_s
     ].json_data()["properties"].keys()
 
 
+def test_table_zoom(gebieden_schema: DatasetSchema):
+    bouwblokken = gebieden_schema.get_table_by_id("bouwblokken")
+    buurten = gebieden_schema.get_table_by_id("buurten")
+
+    # bouwblokken has a zoom min and max set
+    assert bouwblokken.min_zoom == 20
+    assert bouwblokken.max_zoom == 28
+    # buurten has no zoom set, and therefore uses the defaults
+    assert buurten.min_zoom == 15
+    assert buurten.max_zoom == 30
+
+
 def test_datasetversions(schema_loader):
     """
     Test dataset versioning results in multiple versions on the dataset and
