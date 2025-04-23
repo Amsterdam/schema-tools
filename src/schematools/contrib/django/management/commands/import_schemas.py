ceb8899fe92e29440c3a2d33b4cb5b31d791c19a
Pieter Stokkink <p.stokkink@amsterdam.nl>
2025-04-23 10:13:21 +0200

    Autopatch database tables (#597)
    * add --dry-run flag to import schemas command
    
    * ensure that we do not alter the database downstream in create_tables, create_views and Dataset.create_for_schema,.save_for_schema,.save_path
    
    * move migration helper functions to separate file
    
    * add migration to import_schemas
    
    * update and add tests
    
    * bump version to 7.5.0
    
    * Update src/schematools/contrib/django/management/commands/create_tables.py
    
    Co-authored-by: Maguell Sandifort <maguell.sandifort@amsterdam.nl>
    
    ---------
    
    Co-authored-by: Maguell Sandifort <maguell.sandifort@amsterdam.nl>

--- src/schematools/contrib/django/management/commands/import_schemas.py
+++ src/schematools/contrib/django/management/commands/import_schemas.py
@@ -1,11 +1,14 @@
 from __future__ import annotations
 
+from collections import deque
 from pathlib import Path
 
 from django.conf import settings
 from django.core.management import BaseCommand
 from django.db.models import Q
 
+from schematools.contrib.django.factories import schema_models_factory
+from schematools.contrib.django.management.commands.migration_helpers import migrate
 from schematools.contrib.django.models import Dataset
 from schematools.loaders import FileSystemSchemaLoader, get_schema_loader
 from schematools.naming import to_snake_case
@@ -33,30 +36,63 @@ class Command(BaseCommand):
         parser.add_argument("--create-tables", dest="create_tables", action="store_true")
         parser.add_argument("--create-views", dest="create_views", action="store_true")
         parser.add_argument("--no-create-tables", dest="create_tables", action="store_false")
+        parser.add_argument("--dry-run", dest="dry_run", action="store_true")
         parser.set_defaults(create_tables=False)
         parser.set_defaults(create_views=False)
 
     def handle(self, *args, **options):
-        if options["schema"]:
-            datasets = self.import_from_files(options["schema"])
-        else:
-            datasets = self.import_from_url(options["schema_url"])
+        self.dry_run = options["dry_run"]
+        self.verbosity = options["verbosity"]
+        self.schema_dependencies = deque()
+        current_datasets = {
+            Dataset.name_from_schema(ds.schema): ds for ds in Dataset.objects.all()
+        }
 
-        if not datasets:
+        if options["schema"]:
+            schemas = self.get_schemas_from_files(options["schema"])
+        else:
+            schemas = self.get_schemas_from_url(options["schema_url"])
+
+        # Contains unsaved Dataset objects if dry_run.
+        updated_datasets = self._run_import(schemas)
+        if not updated_datasets:
             self.stdout.write("No new datasets imported")
             return
+
+        # Loop over updated datasets and perform migrations.
+        for updated_dataset in updated_datasets:
+            current_dataset = current_datasets.get(
+                Dataset.name_from_schema(updated_dataset.schema)
+            )
+            if not current_dataset:  # New dataset
+                continue
+
+            real_apps = self._load_dependencies(updated_dataset.schema, updated_dataset)
+            for current_table in current_dataset.schema.tables:
+                updated_table = updated_dataset.schema.get_table_by_id(
+                    current_table.id, include_nested=False, include_through=False
+                )
+                migrate(
+                    self,
+                    current_dataset,
+                    updated_dataset,
+                    current_table,
+                    updated_table,
+                    real_apps,
+                    dry_run=self.dry_run,
+                )
 
         # Reasons for not creating tables directly are to manually configure the
         # "Datasets" model flags first. E.g. disable "enable_db".
         if options["create_tables"]:
-            create_tables(self, datasets, allow_unmanaged=True)
+            create_tables(self, updated_datasets, allow_unmanaged=True, dry_run=self.dry_run)
 
         if options["create_views"]:
-            create_views(self, datasets)
+            create_views(self, updated_datasets, dry_run=self.dry_run)
 
-    def import_from_files(self, schema_files) -> list[Dataset]:
+    def get_schemas_from_files(self, schema_files) -> dict[str, DatasetSchema]:
         """Import all schema definitions from the given files."""
-        datasets = []
+        schemas = {}
         shared_loaders = {}
 
         def _get_shared_loader(path):
@@ -75,7 +111,7 @@ class Command(BaseCommand):
                 # As intended, read all datasets
                 # The folder might be a sub path in the repository,
                 # in which case only a few datasets will be imported.
-                files = _get_shared_loader(file).get_all_datasets()
+                schemas.update(_get_shared_loader(file).get_all_datasets())
             else:
                 # Previous logic also allowed selecting a single file by name.
                 # This still needs to resolve the root to resolve relations,
@@ -88,24 +124,53 @@ class Command(BaseCommand):
                 if str(path) == ".":
                     path = dataset.id  # workaround for unit tests
 
-                files = {path: dataset}
+                schemas.update({path: dataset})
 
-            datasets.extend(self._run_import(files))
+        return schemas
 
-        return datasets
-
-    def import_from_url(self, schema_url) -> list[Dataset]:
+    def get_schemas_from_url(self, schema_url) -> dict[str, DatasetSchema]:
         """Import all schema definitions from a URL"""
         self.stdout.write(f"Loading schema from {schema_url}")
         loader = get_schema_loader(schema_url, loaded_callback=self._loaded_callback)
-        return self._run_import(loader.get_all_datasets())
+        return loader.get_all_datasets()
 
     def _loaded_callback(self, schema: DatasetSchema):
+        """Track which schema's get loaded. This is also used for dependency tracking."""
         self.stdout.write(f"* Loaded {schema.id}")
 
-    def _run_import(self, files: dict[str, DatasetSchema]) -> list[Dataset]:
+        self.schema_dependencies.append(schema)
+
+    def _load_dependencies(self, dataset_schema: DatasetSchema, dataset) -> list[str]:
+        """Make sure any dependencies are loaded.
+
+        Returns the list of "real app names", which tells Django migrations those apps
+        are not part of the project state, but can be found in the main app registry itself.
+        """
+        related_ids = dataset_schema.related_dataset_schema_ids
+        real_apps = []
+
+        # Load first, and this fills the cache.
+        for dataset_id in related_ids - {dataset_schema.id}:
+            dataset_schema.loader.get_dataset(dataset_id, prefetch_related=True)
+
+        # Turn any loaded schema into a model.
+        # And when a call to model_factory() triggers loading of more schemas,
+        # these are also picked up from the deque() collection object.
+        while self.schema_dependencies:
+            dependency_schema = self.schema_dependencies.popleft()
+            if dependency_schema.id == dataset_schema.id:
+                continue
+
+            if self.verbosity >= 2:
+                self.stdout.write(f"-- Building models for {dependency_schema.id}")
+            schema_models_factory(dataset)
+            real_apps.append(dependency_schema.id)
+
+        return real_apps
+
+    def _run_import(self, dataset_schemas: dict[str, DatasetSchema]) -> list[Dataset]:
         datasets = []
-        for path, schema in files.items():
+        for path, schema in dataset_schemas.items():
             self.stdout.write(f"* Processing {schema.id}")
             dataset = self._import(schema, path)
             if dataset is not None:
@@ -125,7 +190,7 @@ class Command(BaseCommand):
                 )
             except Dataset.DoesNotExist:
                 # Give up, Create new dataset
-                dataset = Dataset.create_for_schema(schema, path)
+                dataset = Dataset.create_for_schema(schema, path, save=not self.dry_run)
                 self.stdout.write(f"  Created {schema.id}")
                 return dataset
 
@@ -133,8 +198,8 @@ class Command(BaseCommand):
         if dataset.is_default_version != schema.is_default_version:
             self.update_dataset_version(dataset, schema)
 
-        updated = dataset.save_for_schema(schema)
-        dataset.save_path(path)
+        updated = dataset.save_for_schema(schema, save=not self.dry_run)
+        dataset.save_path(path, save=not self.dry_run)
         return dataset if updated else None
 
     def update_dataset_version(self, dataset: Dataset, schema: DatasetSchema) -> None:
@@ -150,8 +215,8 @@ class Command(BaseCommand):
                 # Update current default dataset name to expected name.
                 if current_default.version:
                     current_default.name = to_snake_case(f"{schema.id}_{current_default.version}")
-
-                    current_default.save()
+                    if not self.dry_run:
+                        current_default.save()
 
         dataset.name = Dataset.name_from_schema(schema)
         dataset.is_default_version = schema.is_default_version
