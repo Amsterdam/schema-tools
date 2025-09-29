22fbaca5108f3aff422fff800b97ff3aecf704ca
JonaBenja <j.bosman@amsterdam.nl>
2025-09-29 12:03:57 +0200

    Add tests for import_scope and import_publisher commands

---
+++ tests/django/test_import_publishers.py
@@ -0,0 +1,15 @@
+from __future__ import annotations
+
+import pytest
+from django.core.management import call_command
+
+from schematools.contrib.django import models
+
+
+@pytest.mark.django_db
+def test_import_schema(here):
+    path = here / "files/publishers/GLEBZ.json"
+    args = [path]
+    call_command("import_publishers", *args)
+    assert models.Publisher.objects.count() == 1
+    assert models.Publisher.objects.first().name == "GLEBZ"
---
+++ tests/django/test_import_scopes.py
@@ -0,0 +1,15 @@
+from __future__ import annotations
+
+import pytest
+from django.core.management import call_command
+
+from schematools.contrib.django import models
+
+
+@pytest.mark.django_db
+def test_import_schema(here):
+    path = here / "files/scopes/GLEBZ/glebzscope.json"
+    args = [path]
+    call_command("import_scopes", *args)
+    assert models.Scope.objects.count() == 1
+    assert models.Scope.objects.first().name == "GLEBZ"
