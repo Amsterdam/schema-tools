666a780186a3e5dac36013889b8c53056023409b
JonaBenja <j.bosman@amsterdam.nl>
2026-09-03 13:45:58 +0200

    Add fixtures for testing main geo is relation

--- tests/conftest.py
+++ tests/conftest.py
@@ -268,6 +268,11 @@ def meetbouten_export_schema(export_schema_loader) -> DatasetSchema:
     """A dataset schema for export tests."""
     return export_schema_loader.get_dataset_from_file("meetbouten/dataset.json")
 
+@pytest.fixture()
+def maingeo_relation_export_schema(export_schema_loader) -> DatasetSchema:
+    """A dataset schema for export tests."""
+    return export_schema_loader.get_dataset_from_file("maingeo_relation/dataset.json")
+
 
 @pytest.fixture()
 def fietspaaltjes_export_schema(export_schema_loader) -> DatasetSchema:
